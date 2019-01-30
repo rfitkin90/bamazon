0b5895931a95d2637309e17e6b36b9b29cff3272
rfitkin90 <rfitkin90@gmail.com>
2019-01-30 07:39:15 -0800

    fixed most bugs; added readme

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # bamazon
+
+`USER INSTRUCTIONS`
+
+### APP 1: bamazoncustomer.js ###
+
+1. Download, install, and run MAMP: https://www.mamp.info/en/downloads/
+
+2. Download, install, and run MySQL Workbench: https://dev.mysql.com/downloads/workbench/
+
+3. In MySQL Workbench, create a new connection by clicking the + sign here:
+
+[NewConnection](https://puu.sh/CEBP2.png)
+
+4. Name the connection "localhost" and the password/user name both to "root":
+
+[ConnectionName](https://puu.sh/CEBT2.png)
+
+5. Test the connection to make sure it's working(if it isn't, make sure MAMP is running):
+
+[TestConnection](https://puu.sh/CEBTB.png)
+
+6. Navigate to your repository directory in your terminal.
+
+7. Install the app's dependencies by typing these 3 commands:
+
+    npm install mysql
+    npm install inquirer
+    npm install colors
+
+[InstallDependencies](https://puu.sh/CEBXX.png)
+
+8. Run the app by typing this command:
+
+    node bamazoncustomer.js
+
+    * This will display a list of products to buy.
+
+[ProductDisplay](https://puu.sh/CEC0B.png)
+
+7. Follow the instructions in the remaining prompts to select which product to buy, how many
+of it you wish to buy, view your total checkout price, and continue shopping or not.
+
+[Prompts](https://puu.sh/CEC5l.png)
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var colors = require("colors");
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -21,11 +22,18 @@ function readProducts() {
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
         // Log all results of the SELECT statement
-        console.log(res);
+        res.forEach(elem => {
+            var elemData = [
+                '     Product ID: ' + `${elem.item_id}`.yellow,
+                '           Name: ' + `${elem.product_name}`.green,
+                '     Department: ' + `${elem.department_name}`.green,
+                '          Price: ' + `$${elem.price}`.yellow,
+                'Amount in stock: ' + `${elem.stock_quantity}`.yellow
+            ].join('\n');
+            console.log(elemData + '\n');
+        });
         // prompt user for product ID
         idPrompt(res);
-
-
     });
 }
 
@@ -40,9 +48,9 @@ function idPrompt(res) {
         ])
         .then(answers => {
             var ID = Number(answers.ID)
-            if (!(ID > 0 || ID <= res.length)) {
+            if (!(ID > 0 && ID <= res.length)) {
                 // if user enters an invalid product ID, notify them and run the same prompt again
-                console.log('Invalid product ID.');
+                console.log('Error: Invalid product ID.'.red);
                 idPrompt(res);
             } else {
                 // if user enters a valid ID, ask them the amount they wish to buy
@@ -53,41 +61,96 @@ function idPrompt(res) {
 }
 
 function purchaseAmountPrompt(res, ID) {
+    var stockQuantity = res[ID - 1].stock_quantity;
     inquirer
         .prompt([
             {
                 type: "input",
-                message: `How many would you like to buy? (${res[ID - 1].stock_quantity} currently in stock)`,
+                message: 'How many would you like to buy? (' +
+                    `${stockQuantity}`.yellow + ' currently in stock):',
                 name: "purchaseAmount"
             }
         ])
         .then(answers => {
             var amount = Number(answers.purchaseAmount);
-            if (typeof amount !== 'number' || amount < 1) {
+            // (!(ID > 0 && ID <= res.length))
+            if (!(amount > 0)) {
                 // if user enters a non-number or negative number, notify them and reprompt
-                console.log('Invalid entry. Please enter a number greater than 0.');
-                purchaseAmountPrompt(ID);
-            } else if (amount > res[ID - 1].stock_quantity) {
-                console.log(`Sorry, we only have ${amount} in stock. Please enter a lower number.`);
-                purchaseAmountPrompt(ID);
+                console.log('Error: Invalid entry. Please enter a number greater than 0.'.red);
+                purchaseAmountPrompt(res, ID);
+            } else if (amount > stockQuantity) {
+                // if they try to purchase more than what's in stock
+                console.log('Sorry, we only have ' + `${stockQuantity}`.yellow +
+                    ' in stock. Please enter a lower number.');
+                purchaseAmountPrompt(res, ID);
             } else {
-                updateProduct(ID, res[ID - 1].stock_quantity, amount);
+                // ask them to confirm their purchase
+                confirmPurchase(res, ID, stockQuantity, amount);
             }
         });
     ;
 }
 
-// don't forget to pass the other item info as paramters to log them
-function updateProduct(ID, stockQuant, amount) {
+function confirmPurchase(res, ID, stockQuantity, amount) {
+    var productName = res[ID - 1].product_name;
+    var individualPrice = res[ID - 1].price;
+    var totalPrice = individualPrice * amount;
+    inquirer
+        .prompt([
+            {
+                type: "confirm",
+                message: 'Purchase ' + `${amount} `.yellow + `${productName} `.green +
+                    'for ' + `$${totalPrice}`.yellow + '?',
+                name: "confirmPurchase"
+            }
+        ])
+        .then(answers => {
+            if (answers.confirmPurchase) {
+                // update the stock quantity in the database if they confirm purchase
+                updateProduct(res, ID, stockQuantity, amount);
+                console.log(`${amount} `.yellow + `${productName} `.green +
+                    'purchased for ' + `$${totalPrice}`.yellow + '!');
+                // ask them if they want to continue shopping
+                reShop(res);
+            } else {
+                // if they don't confirm, ask them if they want to buy another product
+                reShop(res);
+            }
+        });
+    ;
+}
+
+/*
+QUESTION:
+why does the app automatically end immediately after calling the reshop prompt at line 114?
+*/
+
+function updateProduct(ID, stockQuantity, amount) {
+    // update stock quantity in database
     connection.query(`UPDATE products
     SET stock_quantity = ?
     WHERE item_id = ?
-    `, [stockQuant - amount, ID], function (err, res) {
+    `, [stockQuantity - amount, ID], function (err, res) {
             if (err) throw err;
-            // console.log(res);
-            console.log('')
         });
     ;
+}
 
-
+function reShop(res) {
+    inquirer
+        .prompt([
+            {
+                type: "confirm",
+                message: "Continue shopping?",
+                name: "reShop"
+            }
+        ])
+        .then(answers => {
+            if (answers.reShop) {
+                idPrompt(res);
+            } else {
+                console.log('Thank you for shopping.');
+            }
+        });
+    ;
 }
