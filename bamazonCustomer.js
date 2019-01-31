b6aceb8c570d0352c47715375cc6ca87c94f2e05
Richard Fitkin <rfitkin90@gmail.com>
2019-01-30 21:11:20 -0800

    1-30 class finished

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -107,7 +107,7 @@ function confirmPurchase(res, ID, stockQuantity, amount) {
         .then(answers => {
             if (answers.confirmPurchase) {
                 // update the stock quantity in the database if they confirm purchase
-                updateProduct(res, ID, stockQuantity, amount);
+                updateProduct(ID, stockQuantity, amount);
                 console.log(`${amount} `.yellow + `${productName} `.green +
                     'purchased for ' + `$${totalPrice}`.yellow + '!');
                 // ask them if they want to continue shopping
@@ -119,11 +119,6 @@ function confirmPurchase(res, ID, stockQuantity, amount) {
         });
     ;
 }
-
-/*
-QUESTION:
-why does the app automatically end immediately after calling the reshop prompt at line 114?
-*/
 
 function updateProduct(ID, stockQuantity, amount) {
     // update stock quantity in database
@@ -137,6 +132,7 @@ function updateProduct(ID, stockQuantity, amount) {
 }
 
 function reShop(res) {
+    console.log('reshop called');
     inquirer
         .prompt([
             {
@@ -146,6 +142,7 @@ function reShop(res) {
             }
         ])
         .then(answers => {
+            console.log("yo")
             if (answers.reShop) {
                 idPrompt(res);
             } else {
---
+++ bamazonManager.js
@@ -0,0 +1,97 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+var colors = require("colors");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "bamazon"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log(`connected as id ${connection.threadId}\n`);
+    menuPrompt();
+});
+
+function menuPrompt() {
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                message: "What would you like to do?",
+                name: "managerActions",
+                choices: [
+                    "View products for sale",
+                    "View low inventory",
+                    "Add to inventory",
+                    "Add new product"
+                ]
+            }
+        ])
+        .then(answers => {
+            if (answers.managerActions === "View products for sale") {
+                viewProducts();
+            } else if (answers.managerActions === "View low inventory") {
+                viewLowInventory();
+            }
+        });
+    ;
+}
+
+function viewProducts() {
+    console.log("Displaying all products...\n");
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        // Log all results of the SELECT statement
+        res.forEach(elem => {
+            var elemData = [
+                '     Product ID: ' + `${elem.item_id}`.yellow,
+                '           Name: ' + `${elem.product_name}`.green,
+                '     Department: ' + `${elem.department_name}`.green,
+                '          Price: ' + `$${elem.price}`.yellow,
+                'Amount in stock: ' + `${elem.stock_quantity}`.yellow
+            ].join('\n');
+            console.log(elemData + '\n');
+        });
+        menuPrompt();
+    });
+}
+
+function viewLowInventory() {
+    console.log("Displaying products with inventories lower than 5000...\n");
+    connection.query("SELECT * FROM products WHERE stock_quantity < ?", [5000], function (err, res) {
+        if (err) throw err;
+        // Log all results of the SELECT statement
+        res.forEach(elem => {
+            var elemData = [
+                '     Product ID: ' + `${elem.item_id}`.yellow,
+                '           Name: ' + `${elem.product_name}`.green,
+                '     Department: ' + `${elem.department_name}`.green,
+                '          Price: ' + `$${elem.price}`.yellow,
+                'Amount in stock: ' + `${elem.stock_quantity}`.yellow
+            ].join('\n');
+            console.log(elemData + '\n');
+        });
+        menuPrompt();
+    });
+}
+
+function addToInventory() {
+    
+}
+
+// function 
+//   * If a manager selects `View Products for Sale`, the app should list every available item:
+//   the item IDs, names, prices, and quantities.
+
+//   * If a manager selects `View Low Inventory`, then it should list all items with an inventory 
+//   count lower than five.
+
+//   * If a manager selects `Add to Inventory`, your app should display a prompt that will let the 
+//   manager "add more" of any item currently in the store.
+
+//   * If a manager selects `Add New Product`, it should allow the manager to add a completely new 
+//   product to the store.
